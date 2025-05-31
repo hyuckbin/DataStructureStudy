3c99014de2a65850f701e9bcd8705d9f8153c6a1
hyuckbin <rpdak159@naver.com>
2025-05-31 19:05:30 +0900

    배열스택 구현

---
+++ DS/stack/ArrayStack.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Stack{
+    int top , capacity;
+    int *list;
+
+    public:
+        Stack(int size) : top(-1), capacity(size){
+            list = new int[size];
+        }
+        ~Stack(){
+            delete[] list;
+        }
+        void push(int val){
+            if(isFull()) return;
+
+            list[++top] = val; 
+        }
+        int pop(){
+            if(isEmpty()) return -1;
+
+            int val = list[top--];
+            return val;
+        }
+        int peek(){
+            if(isEmpty()) return -1;
+
+            return list[top];
+        }
+        bool isEmpty(){
+            return top==-1;
+        }
+        bool isFull(){
+            return top == capacity-1;
+        }
+        void print(){
+            int i = top;
+            while(true){
+                cout << list[i--] << "->" ;
+
+                if(i == -1) break;
+            }
+            cout<<"null" <<endl;
+        }
+};
+
+int main(){
+    Stack s(5);
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    s.print();
+
+    s.pop();
+    s.pop();
+
+    s.print();
+
+    s.push(13);
+    s.push(14);
+
+    s.print();
+
+    return 0;
+}
