6c05e4b1fc74c11dd61c0f3be113d171f5774621
hyuckbin <rpdak159@naver.com>
2025-05-31 23:18:04 +0900

    단일 연결 리스트 2번째 완성

--- DS/stack/ListStack.cpp
+++ DS/stack/ListStack.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
 using namespace std;
 
+struct Node{
+    int data;
+    Node* next;
 
+    Node(int val): data(val) , next(nullptr) {}
+};
 
+class Stack{
+    Node* head;
 
+    public :
+        Stack() : head(nullptr){}
+        ~Stack(){
+            clear();
+        }
+        void push(int val){
+            Node* newnode = new Node(val);
+            newnode->next = head;
+            head = newnode;
+        }
+        int pop(){
+            int a = head -> data;
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+
+            return a;
+        }
+        int peek(){
+            return head->data;
+        }
+        bool isEmpty(){
+            return head == nullptr;
+        }
+        void display(){
+            if(isEmpty()) return;
+            Node* curr = head;
+            while(true){
+                cout << curr->data << "-->";
+                curr = curr->next;
+
+                if(curr== nullptr) break;
+            }
+            cout <<"null" <<endl;
+
+        }
+        void clear(){
+            while(head!= nullptr){
+                Node* temp = head;
+                head = head -> next;
+                delete temp;
+            }
+        }
+};
 int main(){
     Stack s;
 
@@ -11,12 +62,16 @@ int main(){
     s.push(20);
     s.push(30);
 
-    s.print();
+    s.display();
 
     s.pop();
     s.pop();
     
-    s.print();
+    s.display();
 
+    s.push(20);
+    s.push(30);
+
+    s.display();
     return 0;
 }
