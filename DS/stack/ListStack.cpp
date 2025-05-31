ad394ceeabf62913d4fddca5c0f3860974a3c1ad
hyuckbin <rpdak159@naver.com>
2025-05-31 23:02:46 +0900

    다시

--- DS/stack/ListStack.cpp
+++ DS/stack/ListStack.cpp
@@ -1,53 +1,8 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
-    int data;
-    Node *next;
 
-    Node(int val) : data(val) , next(nullptr) {}
-};
 
-class Stack{
-    Node* head;
-    int size;
-
-    public:
-        Stack(): head(nullptr),size(0){}
-       
-        void push(int val){
-            Node *new_node = new Node(val);
-            new_node->next = head;
-            head = new_node;
-            size++;
-        }
-        int pop(){
-            if(isEmpty()) return -1;
-
-            Node* temp = head;
-            int v = temp->data;
-            head = head->next;
-            delete temp;
-            size--;
-            
-            return v;
-        }
-        bool isEmpty(){
-            return head == nullptr;
-        }
-
-        void print() {
-        if (isEmpty()) return;
-    
-        Node* current = head;
-        cout << "Stack: ";
-        while (current != nullptr) {
-            cout << current->data << " -> ";
-            current = current->next;
-        }
-        cout << "null" << endl;
-}
-};
 
 int main(){
     Stack s;
