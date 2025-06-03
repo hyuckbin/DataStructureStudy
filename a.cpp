451bd097cb2d989572782d84aa561b15e203d43f
hyuckbin <rpdak159@naver.com>
2025-06-03 14:57:02 +0900

    switch

---
+++ a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int choice;
+    bool digit = true;
+while(true){
+    cin >> choice;
+    switch(choice){
+        case 1 : {
+            cout << "hello";
+            break;
+        }
+
+        case 2 :{
+            
+        cout << "world";
+        break;
+            
+    }
+}
+if(choice != 1 && choice !=2) break;
+}
+    return 0;
+}
